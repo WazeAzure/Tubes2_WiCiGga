e1eb422a6ec67b68d47ef07a3ccc792b55b08d95
Akmal2205 <syarafiakml2004@gmail.com>
2024-04-20 16:03:55 +0700

    feat : autocomplete

--- src/FrontEnd/wicigga_frontend/src/App.js
+++ src/FrontEnd/wicigga_frontend/src/App.js
@@ -3,18 +3,9 @@ import { useState, useEffect } from 'react';
 
 
 function App() {
-  const [data, setData] = useState([]);
-  // testing using state
-
-  useEffect(() => {
-    fetch('http://localhost:4000/path')
-      .then((res) => {
-        return res.json();
-      }).then((data) => {
-        console.log(data);
-        setData(data.path);
-      })
-  }, [])
+  //Autocomplete data
+  const [data1, setData1] = useState([]);
+  const [data2, setData2] = useState([]);
 
   //Search Bar Handler
   const [value1, setValue1] = useState("");
@@ -23,7 +14,7 @@ function App() {
   //Toggle Button Handler, false = BFS, true = IDS
   const [buttonState, setButtonState] = useState(false);
 
-
+  //Input handler
   const onChange1 = (event) => {
     setValue1(event.target.value);
   }
@@ -32,8 +23,39 @@ function App() {
     setValue2(event.target.value);
   }
 
+  //Fetch data for autocomplet from wikipedia's API
+  useEffect(() => {
+    if (value1 !== "") {
+      fetch("https://en.wikipedia.org/w/api.php?action=query&origin=*&prop=extracts&format=json&formatversion=2&list=search&srsearch=" + value1)
+        .then((res) => {
+          return res.json();
+        }).then((jsonDat) => {
+          console.log(jsonDat.query);
+          return jsonDat.query;
+        }).then((que) => {
+          console.log(que.search.map(item => item.title));
+          setData1(que.search.map(item => item.title));;
+        });
+    }
+  }, [value1])
+
+  useEffect(() => {
+    if (value2 !== "") {
+      fetch("https://en.wikipedia.org/w/api.php?action=query&origin=*&prop=extracts&format=json&formatversion=2&list=search&srsearch=" + value2)
+        .then((res) => {
+          return res.json();
+        }).then((jsonDat) => {
+          console.log(jsonDat.query);
+          return jsonDat.query;
+        }).then((que) => {
+          console.log(que.search.map(item => item.title));
+          setData2(que.search.map(item => item.title));;
+        });
+    }
+  }, [value2])
+
+  //Search Button Handler
   const onSearch = (searchTerm) => {
-    //fetch api
     console.log("search", searchTerm);
   }
 
@@ -49,17 +71,17 @@ function App() {
             <input type="text" className='search-bar' value={value1} onChange={onChange1} />
           </div>
           <div className='dropdown-offset'>
-            <div className={'dropdown' + (data.some(item => {
+            <div className={'dropdown' + (data1.some(item => {
               const searchTerm = value1.toLowerCase();
               const pathString = item.toLowerCase();
-              return searchTerm && pathString.startsWith(searchTerm) && searchTerm !== pathString;
+              return searchTerm && pathString.startsWith(searchTerm);
             }) ? '' : 'dummy')}>
-              {data.filter(item => {
+              {data1.filter(item => {
                 const searchTerm = value1.toLowerCase();
                 const pathString = item.toLowerCase();
 
-                return searchTerm && pathString.startsWith(searchTerm) && searchTerm !== pathString;
-              })
+                return searchTerm && pathString.startsWith(searchTerm);
+              }).slice(0, 5)
                 .map((item) => (
                   <li className="search-result">{item}</li>
                 ))
@@ -75,12 +97,12 @@ function App() {
             <input type="text" className='search-bar' value={value2} onChange={onChange2} />
           </div>
           <div className='dropdown-offset'>
-            <div className={'dropdown' + (data.some(item => {
+            <div className={'dropdown' + (data2.some(item => {
               const searchTerm = value2.toLowerCase();
               const pathString = item.toLowerCase();
               return searchTerm && pathString.startsWith(searchTerm) && searchTerm !== pathString;
             }) ? '' : 'dummy')}>
-              {data.filter(item => {
+              {data2.filter(item => {
                 const searchTerm = value2.toLowerCase();
                 const pathString = item.toLowerCase();
 
@@ -105,27 +127,3 @@ function App() {
 }
 
 export default App;
-
-{/* <div className='search-container'>
-        <div className='search-bar-outer'>
-          <div className='search-bar'>
-            <input type="text" value={value1} onChange={onChange1} />
-          </div>
-          <div className={'dropdown' + (data.some(item => {
-            const searchTerm = value1.toLowerCase();
-            const pathString = item.toLowerCase();
-            return searchTerm && pathString.startsWith(searchTerm) && searchTerm !== pathString;
-          }) ? '' : '-empty')}>
-            {data.filter(item => {
-              const searchTerm = value1.toLowerCase();
-              const pathString = item.toLowerCase();
-
-              return searchTerm && pathString.startsWith(searchTerm) && searchTerm !== pathString;
-            })
-              .map((item) => (
-                <li className="search-result">{item}</li>
-              ))
-            }
-          </div>
-        </div>
-      </div> */}
