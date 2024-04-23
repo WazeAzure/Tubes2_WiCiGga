c83266a7ae87bc9819801b68650b3a1165664395
WazeAzure <bryan.rich0604@gmail.com>
2024-04-23 17:33:22 +0700

    error: BFS not finished

--- src/BackEnd/BFS.go
+++ src/BackEnd/BFS.go
@@ -2,18 +2,22 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"sync"
 	"time"
 )
 
+var visited_bfs = make(map[string]bool)
 var (
 	url_queue   []string
-	visited_bfs map[string]string
 	mutex       sync.Mutex
 	wg          sync.WaitGroup
-	slave       int
 	current_url string
 )
+
+func randomTime() time.Duration {
+	return 50 + time.Duration(rand.Intn(250))
+}
 
 func bfsHandler(url string, end string) *ResponseAPI {
 	var resp ResponseAPI
@@ -21,52 +25,40 @@ func bfsHandler(url string, end string) *ResponseAPI {
 	defer timeTrack(time.Now(), "scrapWeb", &resp.Time)
 
 	current_url = url
-	BFS(current_url, end, &resp)
-	current_url = url_queue[0]
-	url_queue = url_queue[1:]
 
 	for current_url != end {
-		if len(url_queue) != 0 {
-			go BFS(current_url, end, &resp)
-			// give 100 ms delay
-			time.Sleep(50 * time.Millisecond)
+
+		go BFS(current_url, end, &resp)
+		// time.Sleep(time.Millisecond * randomTime())
+
+		if len(url_queue) > 0 {
 			current_url = url_queue[0]
 			url_queue = url_queue[1:]
 		}
 	}
-
-	if current_url == end {
-		resp.Status = true
-		resp.Message = "Path Found"
-	} else {
-		resp.Status = false
-		resp.Message = "No Possible Path Found"
-	}
-
-	fmt.Println("=======[ END ]=======")
-	fmt.Println(current_url)
 
 	return &resp
 }
 
 func BFS(current_url string, end string, resp *ResponseAPI) {
 
-	// fmt.Println(current_url)
+	fmt.Println(current_url)
 
 	link_res := scrapWeb(current_url)
+
 	for _, elmt := range link_res {
-		// check if it not exist
-		if !isIn(elmt, url_queue) {
-			url_queue = append(url_queue, elmt)
-		}
+		// check if
+		go func() {
+			mutex.Lock()
+			_, err := visited_bfs[elmt]
+			mutex.Unlock()
+			if !err {
+				// not exist
+				mutex.Lock()
+				visited_bfs[elmt] = true
+				url_queue = append(url_queue, elmt)
+				mutex.Unlock()
+			}
+		}()
 	}
-
-	if current_url == end {
-		resp.Status = true
-		resp.Message = "Path Found"
-		fmt.Println("====================================")
-		fmt.Println("------------ FOUND -----------------")
-		fmt.Println("====================================")
-	}
-
 }
